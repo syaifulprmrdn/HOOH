925e40fce66c70664cc2ea466695a97952a544d2
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:59:53 +0700

    Update CELENG1.py

--- pages/CELENG1.py
+++ pages/CELENG1.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title="apakah musim depan tsunami trophy?", layout="centered")
-st.sidebar.header("celeng1")
+
 st.set_page_config(page_title="Mancester is blu")
 st.title("Mancester is blu")
 st.header("Mancester is blu")
-st.title("16")
-st.write("16")
+st.title("STD!")
+st.write("MATARAM IS RED MATARAM IS BLUE")
+st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True
 st.image ("https://cdns.klimg.com/mav-prod-resized/480x/webp/feedImage/2024/9/30/1727686979579-3utb6.jpeg", caption=" HAAAAAHHHH!", use_column_width=True)
