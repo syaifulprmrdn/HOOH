1ac91b91a44f9355287cf0800768a1677b3bd216
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:38:22 +0700

    Create CELENG2.py

---
+++ PAGES/CELENG2.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+
+
+
+st.set_page_config(page_title="Mancester is blek")
+st.title("Mancester is blek")
+st.header("Mancester is blek")
+st.title("inpo throphy")
+st.write("16")
+st.image ("https://cdns.klimg.com/mav-prod-resized/720x/webp/newsOg/2024/9/30/1727687517187-4ujfdi.jpeg", caption=" hooraumum!", use_column_width=True)
