fa4c99709409e7f5ac1da68dd93d7a571c6fc7a2
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 23:17:55 +0700

    Update celeng2.py

--- pages/celeng2.py
+++ pages/celeng2.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("inpo throphy")
 st.write("16")
-st.image ("https://cdns.klimg.com/mav-prod-resized/720x/webp/newsOg/2024/9/30/1727687517187-4ujfdi.jpeg", caption=" hooraumum!", use_column_width=True)
+st.image ("https://drive.google.com/file/d/1QyxRIsh2XfpfUxvl_c5dynAcV5mdFqg1/view?usp=sharing", caption=" hooraumum!", use_column_width=True)
