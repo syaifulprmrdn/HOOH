f21de4b580fa25c5eacba76659a6778b464ace6c
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:45:45 +0700

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="apakah musim depan tsunami trophy?", layout="centered")
+st.sidebar.header("dashboard")
 st.title("STD!")
 st.write("MATARAM IS RED MATARAM IS BLUE")
-st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True)
+st.image ("https://akcdn.detik.net.id/community/media/visual/2024/01/20/meme-andre-onana-3.png?w=600&q=90", caption=" JAWA!", use_column_width=True)
