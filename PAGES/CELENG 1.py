2209c90edf3218946cd2ca342b5eae6e9cf0d0bb
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:37:39 +0700

    Create CELENG 1.py

---
+++ PAGES/CELENG 1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+
+
+
+st.set_page_config(page_title="Mancester is blu")
+st.title("Mancester is blu")
+st.header("Mancester is blu")
+st.title("16")
+st.write("16")
+st.image ("https://cdns.klimg.com/mav-prod-resized/480x/webp/feedImage/2024/9/30/1727686979579-3utb6.jpeg", caption=" HAAAAAHHHH!", use_column_width=True)
