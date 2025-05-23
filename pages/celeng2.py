48ff012f9dc62f19a2a40fe785964cee1e72981b
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:57:37 +0700

    Update celeng2.py

--- pages/celeng2.py
+++ pages/celeng2.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 
 
-
+st.set_page_config(page_title="apakah musim depan tsunami trophy?", layout="centered")
+st.sidebar.header("celeng2")
 st.set_page_config(page_title="Mancester is blek")
 st.title("Mancester is blek")
 st.header("Mancester is blek")
