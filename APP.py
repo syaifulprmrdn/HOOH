f385fe5beaa90d2dc20e580dec111972cbd7218e
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:48:04 +0700

    Update APP.py

--- APP.py
+++ APP.py
@@ -4,4 +4,5 @@ st.set_page_config(page_title="apakah musim depan tsunami trophy?", layout="cent
 st.sidebar.header("dashboard")
 st.title("STD!")
 st.write("MATARAM IS RED MATARAM IS BLUE")
-st.image ("https://akcdn.detik.net.id/community/media/visual/2024/01/20/meme-andre-onana-3.png?w=600&q=90", caption=" JAWA!", use_column_width=True)
+st.write("MATARAM IS RED MATARAM IS BLUE")
+st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True)
